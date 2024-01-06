e6a02ca14eca989c7e9e8b69c801b3d324addf28
toankhung <lyminhtoan2004@gmail.com>
2024-01-06 19:16:07 +0700

    lab1

---
+++ bai3/mvc/model.php
@@ -0,0 +1,16 @@
+<?php
+function getBlogById($id)
+{
+    include "database.php";
+    $sql = "SELECT * FROM blog WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->num_rows > 0)
+    {
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        return $row;
+    }
+
+}
---
+++ bai3/mvc/view.php
@@ -0,0 +1,5 @@
+title: <?= $blog["title"]?>
+<form action="" method="post">
+    <input type="text" name="id">
+    <button type="submit">Submit</button>
+</form>
