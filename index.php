6372d4ebff915f1acb00902e0459fa70f3ced65b
toankhung <lyminhtoan2004@gmail.com>
2024-01-06 15:10:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,34 @@
 <?php
-include "./mvc/controller.php";
+// include "./mvc/controller.php";
+
+$course = [
+    's1' => 'course1',
+    's2' => 'course2',
+    's3' => 'course3'
+];
+
+$list_of_courses = get_courses();
+$semester = (!empty($_GET['semester']) ? $_GET['semester'] : "");
+$course_name = find_by_semester($semester);
+$page_content = $course_name;
+
+function get_courses()
+{
+    global $course;
+    return array_values($course);
+}
+
+function find_by_semester($semester)
+{
+    global $course;
+    return (array_key_exists($semester, $course) ? $course[$semester] : "Invalid course");
+}
+
+?>
+
+<?= $page_content ?>
+<select name="course">
+    <?php foreach ($list_of_courses as $course_name) :  ?>
+        <option value=""><?= $course_name ?></option>
+    <?php endforeach; ?>
+</select>
