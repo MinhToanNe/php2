e5e09847c40bb5e51a2f4fd9c864e40c0925538a
toankhung <lyminhtoan2004@gmail.com>
2024-01-15 15:45:00 +0700

    Assignment_1

--- mvc/view/AdminLayout.php
+++ mvc/view/AdminLayout.php
@@ -1,3 +1,3 @@
-<?php $this->view('blocks/HeaderClient', $data); ?>
+<?php $this->view('blocks/AdminHeader', $data); ?>
 <?php require_once "./mvc/view/pages/" . $data['pages'] . ".php"; ?>
-<?php $this->view('blocks/FooterClient', $data); ?>
+<?php $this->view('blocks/AdminFooter', $data); ?>
---
+++ mvc/view/blocks/HeaderClient.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/public/css/home.css">
+</head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+</head>
+<body>
+    <div class="row header">
+
+    </div>
+    
